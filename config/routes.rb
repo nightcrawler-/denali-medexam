8fda0878818ecf2a7223462c9b1d8292a09cf12a
Frederick Nyawaya <o.frederickn@gmail.com>
2020-04-01 00:27:28 +0300

    added practitioner model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :workplaces
   resources :practitioners
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
