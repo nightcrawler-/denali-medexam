986dcb609fd2f1b5162ca627398ad1d24c87c5ab
Frederick Nyawaya <o.frederickn@gmail.com>
2020-03-31 23:47:14 +0300

    recatored spec

--- spec/models/practitioner_spec.rb
+++ spec/models/practitioner_spec.rb
@@ -2,42 +2,41 @@ require 'rails_helper'
 
 RSpec.describe Practitioner, type: :model do
 
-  subject { described_class.new }
+  subject { described_class.new(
+              name: "Jane",
+              email: "Me",
+              registration: "woah",
+              location: "nyef",
+              address: "miau")
+          }
 
   it "is valid with valid attributes" do
-    
-    subject.name = "Jane"
-    subject.email = "Me"
-    subject.registration = "woah"
-    subject.location = "nyef"
-    subject.address = "miau"
-
     expect(subject).to be_valid
   end
 
   it "is not valid without a name" do
-    practitioner = Practitioner.new(name: nil)
-    expect(practitioner).to_not be_valid
+    subject.name = nil
+    expect(subject).to_not be_valid
   end 
 
   it "is not valid without an email" do
-    practitioner = Practitioner.new(email: nil)
-    expect(practitioner).to_not be_valid
+    subject.email = nil
+    expect(subject).to_not be_valid
   end 
 
   it "is not valid without a registration" do
-    practitioner = Practitioner.new(registration: nil)
-    expect(practitioner).to_not be_valid
+    subject.registration = nil
+    expect(subject).to_not be_valid
   end 
 
   it "is not valid without an address" do
-    practitioner = Practitioner.new(address: nil)
-    expect(practitioner).to_not be_valid
+    subject.address = nil
+    expect(subject).to_not be_valid
   end 
 
   it "is not valid without a location" do
-  practitioner = Practitioner.new(location: nil)
-  expect(practitioner).to_not be_valid
+    subject.location = nil
+  expect(subject).to_not be_valid
   end 
 
 end
