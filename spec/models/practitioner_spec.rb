a789f455b650c6b507c30e36d6ee9576df62355c
Frederick Nyawaya <o.frederickn@gmail.com>
2020-04-01 00:44:45 +0300

    complete workplaces model, rspec for model too

--- spec/models/practitioner_spec.rb
+++ spec/models/practitioner_spec.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: practitioners
+#
+#  id           :bigint           not null, primary key
+#  address      :string
+#  email        :string
+#  fax          :string
+#  location     :string
+#  name         :string
+#  registration :string
+#  signature    :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Practitioner, type: :model do
