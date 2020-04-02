e1fdcdd7d89d039bfdf2a048ac6458459a74d931
Frederick Nyawaya <o.frederickn@gmail.com>
2020-04-02 23:45:37 +0300

    fixes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,11 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+
 require 'rspec/rails'
+
+require 'devise'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -42,6 +46,8 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+  config.include Devise::Test::ControllerHelpers, :type => :controller
+
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
@@ -71,5 +77,5 @@ RSpec.configure do |config|
       with.library :rails
     end
   end
-  
+
 end
