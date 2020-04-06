b829b13dcb46ee6ae21c86f4973d53104b070746
Frederick Nyawaya <o.frederickn@gmail.com>
2020-04-06 15:50:57 +0300

    dosh 9 content

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,21 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
+
+  #root :to => redirect("/users/sign_in")
+  root :to => "practitioners#index"
+
   resources :employee_examination_sessions
   resources :employees
   resources :examination_sessions
   resources :workplaces
-  #root :to => redirect("/users/sign_in")
-  root :to => "practitioners#index"
-
   resources :practitioners
+
+  get 'examination_sessions_summary/:id' => 'examination_sessions#summary'
+
+
   devise_for :users
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
