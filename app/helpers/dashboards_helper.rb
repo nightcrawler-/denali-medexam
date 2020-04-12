d7228725b68386cd24a4e8be8202294233e6bae4
Frederick Nyawaya <o.frederickn@gmail.com>
2020-04-12 19:17:33 +0300

    added dashboard helpers*

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,2 +1,19 @@
 module DashboardsHelper
+
+    def total_employees_registered
+        Employee.all.count
+    end
+
+    def total_workplaces_registered
+        Workplace.all.count
+    end
+
+    def total_individual_examinations_done
+        EmployeeExaminationSession.all.count
+    end
+
+    def total_workplace_examinations_done
+        ExaminationSession.all.count
+    end
+
 end
