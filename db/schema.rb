98cc258180f02d58af05fae2ab8d4d3e6d9d20d4
Frederick Nyawaya <o.frederickn@gmail.com>
2020-04-12 18:43:45 +0300

    rails dashboard core

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_091227) do
+ActiveRecord::Schema.define(version: 2020_04_10_053143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dashboards", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "employee_examination_sessions", force: :cascade do |t|
     t.float "bp"
