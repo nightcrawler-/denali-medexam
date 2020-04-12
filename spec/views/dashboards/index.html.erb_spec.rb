1452ed2e9dbd8899e88711221524771e9ab2082a
Frederick Nyawaya <o.frederickn@gmail.com>
2020-04-13 01:47:54 +0300

    fix dashboard tests

--- spec/views/dashboards/index.html.erb_spec.rb
+++ spec/views/dashboards/index.html.erb_spec.rb
@@ -1,13 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "dashboards/index", type: :view do
-  before(:each) do
-    assign(:dashboards, [
-      Dashboard.create!(),
-      Dashboard.create!()
-    ])
-  end
-
   it "renders a list of dashboards" do
     render
   end
