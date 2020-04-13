71246d523cb59d1e5dbad8511dc55f0d31a053be
Frederick Nyawaya <o.frederickn@gmail.com>
2020-04-13 17:21:13 +0300

    moved to safer non root root user for deployments

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -66,7 +66,7 @@ server '47.57.90.230', port: 22, roles: [:web, :app, :db], primary: true
 
 # set :repo_url,        '[YOUR GIT SSH ADDRESS: git@example.com:username/appname.git]'
 # set :application,     '[APP_NAME]'
-set :user,            'root'
+set :user,            'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
@@ -76,13 +76,13 @@ set :use_sudo,        false
 set :branch,          :develop
 set :stage,           :production
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(/home/frederick/.ssh/fred.pem), auth_methods: %w('publickey') }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(/home/frederick/.ssh/monday.pem), auth_methods: %w('publickey') }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
@@ -136,6 +136,11 @@ namespace :deploy do
       invoke 'puma:restart'
     end
   end
+
+  #desc "Reload the database with seed data"
+  #task :seed do
+  #  run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+  #end
 
   before :starting,     :check_revision
   after  :finishing,    :compile_assets
