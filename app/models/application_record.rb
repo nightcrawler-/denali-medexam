b1c2b89c748f9dbf4fecd3f0adfe05b4d53a353e
Frederick Nyawaya <o.frederickn@gmail.com>
2020-04-13 09:40:44 +0300

    disable pepertrail for all records

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
-  has_paper_trail
+  #has_paper_trail --  temp disable papertrail, too damn large db, exceed available quoa on heroku by far, row and sizewise
 
   self.abstract_class = true
 end
